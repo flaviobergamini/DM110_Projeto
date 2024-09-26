10c3c744728e2c687eb57be274a58c9a86ce4b94
unknown <flaviohenrique@gec.inatel.br>
2024-09-26 00:03:36 -0300

    feat: adding annotations and paths

--- trabalho-impl/src/main/java/br/inatel/trabalho/impl/ProductResource.java
+++ trabalho-impl/src/main/java/br/inatel/trabalho/impl/ProductResource.java
@@ -1,17 +1,36 @@
 package br.inatel.trabalho.impl;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import br.inatel.trabalho.api.product.ProductInterface;
 import br.inatel.trabalho.api.product.ProductTO;
+import br.inatel.trabalho.interfaces.product.ProductLocal;
+import jakarta.ejb.EJB;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
+@RequestScoped
+@Path("/product")
 public class ProductResource implements ProductInterface {
 	
+	@Inject
+	Logger log;
+	
+	@EJB
+	private ProductLocal productBean;
+	
 	@POST
+	@Path("/state")
+	@Produces(MediaType.APPLICATION_JSON)
 	@Override
 	public void saveState(ProductTO product) {
 		// TODO Auto-generated method stub
@@ -19,6 +38,8 @@ public class ProductResource implements ProductInterface {
 	}
 	
 	@GET
+	@Path("/products")
+	@Produces(MediaType.APPLICATION_JSON)
 	@Override
 	public List<ProductTO> getProducts() {
 		// TODO Auto-generated method stub
@@ -26,13 +47,16 @@ public class ProductResource implements ProductInterface {
 	}
 
 	@GET
+	@Path("/product/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
 	@Override
-	public List<ProductTO> getProductById(int id) {
+	public List<ProductTO> getProductById(@PathParam("id") int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
 	@PUT
+	@Path("/update")
 	@Override
 	public void updateProduct(ProductTO product) {
 		// TODO Auto-generated method stub
@@ -40,8 +64,9 @@ public class ProductResource implements ProductInterface {
 	}
 
 	@DELETE
+	@Path("/delete/{id}")
 	@Override
-	public void deleteProduct(int id) {
+	public void deleteProduct(@PathParam("id") int id) {
 		// TODO Auto-generated method stub
 		
 	}
