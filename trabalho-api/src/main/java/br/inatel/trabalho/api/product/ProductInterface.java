6467c7fe1f69bf19eed8ae86e09bf154bf488342
unknown <flaviohenrique@gec.inatel.br>
2024-10-02 01:02:51 -0300

    feat: creating CRUD methods in ProductBean and refsctoring ProductInterface

--- trabalho-api/src/main/java/br/inatel/trabalho/api/product/ProductInterface.java
+++ trabalho-api/src/main/java/br/inatel/trabalho/api/product/ProductInterface.java
@@ -3,9 +3,9 @@ package br.inatel.trabalho.api.product;
 import java.util.List;
 
 public interface ProductInterface {
-	public void saveState(ProductTO product);
-	public String getProducts();
-	public List<ProductTO> getProductById(int id);
+	public void createProduct(ProductTO product);
+	public List<ProductTO> getProducts();
+	public ProductTO getProductByCode(int code);
 	public void updateProduct(ProductTO product);
-	public void deleteProduct(int id);
+	public void deleteProduct(int code);
 }
--- trabalho-ejb/src/main/java/br/inatel/trabalho/product/beans/ProductBean.java
+++ trabalho-ejb/src/main/java/br/inatel/trabalho/product/beans/ProductBean.java
@@ -6,43 +6,84 @@ import java.util.logging.Logger;
 import br.inatel.trabalho.api.product.ProductTO;
 import br.inatel.trabalho.interfaces.product.ProductLocal;
 import br.inatel.trabalho.interfaces.product.ProductRemote;
+import br.inatel.trabalho.product.entities.ProductEntity;
+import br.inatel.trabalho.product.support.ProductConversor;
+import jakarta.ejb.Local;
+import jakarta.ejb.Remote;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 
 @Stateless
+@Local(ProductLocal.class)
+@Remote(ProductRemote.class)
 public class ProductBean implements ProductLocal, ProductRemote {
 	
-	@Inject
+	@Inject 
 	Logger log;
+	
+	@PersistenceContext(unitName = "product_pu")
+	private EntityManager em;
 
 	@Override
-	public void saveState(ProductTO product) {
-		// TODO Auto-generated method stub
+	public void createProduct(ProductTO product) {
+		log.info("Saving Product in Database - Product: " + product.getName());
+		
+		ProductEntity productEnity = ProductConversor.ToProductEntity(product);
+		
+		em.persist(productEnity);
 		
 	}
 
 	@Override
-	public String getProducts() {
-		// TODO Auto-generated method stub
-		return "";
+	public List<ProductTO> getProducts() {
+		log.info("Querying All Products objects");
+		
+		String hql = "SELECT p FROM ProductEntity p";
+		
+		TypedQuery<ProductEntity> query = em.createQuery(hql, ProductEntity.class);
+		
+		return ProductConversor.ToListProductTO(query.getResultList());
 	}
 
 	@Override
-	public List<ProductTO> getProductById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public ProductTO getProductByCode(int code) {
+		ProductEntity product = em.find(ProductEntity.class, code);
+		
+		if (product == null)
+			return null;
+		
+		return ProductConversor.toProductTO(product);
 	}
 
 	@Override
 	public void updateProduct(ProductTO product) {
-		// TODO Auto-generated method stub
+		ProductEntity productEntity = ProductConversor.ToProductEntity(product);
 		
+		if(productEntity != null) {
+			em.merge(productEntity);
+		
+			log.info("Product updated, code " + product.getCode());
+		}
+		else {
+			log.info("Product to update not found, code " + product.getCode());
+		}
 	}
 
 	@Override
-	public void deleteProduct(int id) {
-		// TODO Auto-generated method stub
+	public void deleteProduct(int code) {
+		ProductEntity product = em.find(ProductEntity.class, code);
 		
+		if(product != null) {
+			em.remove(product);
+		
+			log.info("Product removed, code " + product.getCode());
+		}
+		else {
+			log.info("Product to remove not found, code " + product.getCode());
+		}
 	}
 }
