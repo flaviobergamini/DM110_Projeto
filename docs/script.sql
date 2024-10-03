9c75c37575fd6a6266d42634c38e800de2bd28d4
unknown <flaviohenrique@gec.inatel.br>
2024-10-03 00:37:53 -0300

    chore: creating SQL schema, Postman collection and additional informations

--- docs/script.sql
+++ docs/script.sql
@@ -1,2 +1,8 @@
---script do banco
-
+CREATE TABLE Product (
+    code INTEGER NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    description VARCHAR(500) NOT NULL,
+    price FLOAT NOT NULL,
+    category VARCHAR(20) NOT NULL,
+    PRIMARY KEY(code)
+);
